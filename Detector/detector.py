32a1662e8336ed4edb732be6d35b533a74908667
ArtemDzhalilov <88921907+ArtemDzhalilov@users.noreply.github.com>
2024-04-28 23:42:23 +0300

    Add files via upload

---
+++ Detector/detector.py
@@ -0,0 +1,131 @@
+import time
+
+import cv2
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from dsfd.face_detection import build_detector
+from ulfdm.vision.ssd.mb_tiny_fd import create_mb_tiny_fd, create_mb_tiny_fd_predictor
+from ulfdm.vision.ssd.mb_tiny_RFB_fd import create_Mb_Tiny_RFB_fd, create_Mb_Tiny_RFB_fd_predictor
+from ultralytics import YOLO
+import torch
+from torch import nn
+import numpy as np
+import os
+import torchvision
+
+from facenet_pytorch.models.utils.detect_face import extract_face
+def fixed_image_standardization(image_tensor):
+    processed_tensor = (image_tensor - 127.5) / 128.0
+    return processed_tensor
+
+class Detector:
+    def __init__(self, model_type: str, device, image_size, output_size):
+        self.model_type = model_type
+        self.device = device
+        self.image_size = image_size
+        self.output_size = output_size
+        self.margin = 0
+        if model_type == 'mtcnn':
+            self.detector = MTCNN(image_size = image_size, device = device)
+        elif model_type == 'dsfd':
+            self.detector = build_detector(
+                "DSFDDetector",
+                max_resolution=output_size
+            )
+        elif model_type=='ulfdm_small':
+            net = create_mb_tiny_fd(2, is_test=False, device=self.device)
+            self.detector = create_mb_tiny_fd_predictor(net, candidate_size=1000, device=self.device)
+        elif model_type=='ulfdm_big':
+            net = create_Mb_Tiny_RFB_fd(2, is_test=False, device=self.device)
+            self.detector = create_Mb_Tiny_RFB_fd_predictor(net, candidate_size=1000, device=self.device)
+        elif model_type=='yolon':
+            self.detector = YOLO('yolov8n-face.pt', verbose=False)
+        elif model_type=='yolom':
+            self.detector = YOLO('yolov8m-face.pt', verbose=False)
+        elif model_type=='yolol':
+            self.detector = YOLO('yolov8l-face.pt', verbose=False)
+        else:
+            raise Exception("The model type is wrong!")
+        self.trans = torchvision.transforms.Compose([
+            torchvision.transforms.Resize((output_size, output_size)),
+        ])
+
+    def detect_with_extractor(self, image):
+        if self.model_type == 'mtcnn':
+            boxes = self.detector.detect(image)[0]
+        elif self.model_type == 'dsfd':
+            boxes = self.detector.detect(image[:, :, ::-1])[:, :4]
+        elif self.model_type=='ulfdm_small':
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            boxes = self.detector.predict(image, False, 0.7)[0]
+        elif self.model_type=='ulfdm_big':
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            boxes = self.detector.predict(image, False, 0.7)[0]
+
+        elif self.model_type=='yolon':
+            boxes = self.detector.predict(image, show=False)[0].boxes.xyxy
+        elif self.model_type=='yolom':
+            boxes = self.detector.predict(image, show=False)[0].boxes.xyxy
+        elif self.model_type=='yolol':
+            boxes = self.detector.predict(image, show=False)[0].boxes.xyxy
+        if boxes.shape[0]==0:
+            return None, None
+        faces = self.extract(image, boxes, None)
+        if faces is None:
+            return None, None
+        faces = self.trans(faces)
+        if type(faces) == torch.Tensor:
+            faces = faces.numpy()
+        return faces, boxes
+
+    def extract(self, img, batch_boxes, save_path):
+        # Determine if a batch or single image was passed
+        batch_mode = True
+        if (
+                not isinstance(img, (list, tuple)) and
+                not (isinstance(img, np.ndarray) and len(img.shape) == 4) and
+                not (isinstance(img, torch.Tensor) and len(img.shape) == 4)
+        ):
+            img = [img]
+            batch_boxes = [batch_boxes]
+            batch_mode = False
+
+        # Parse save path(s)
+        if save_path is not None:
+            if isinstance(save_path, str):
+                save_path = [save_path]
+        else:
+            save_path = [None for _ in range(len(img))]
+
+        # Process all bounding boxes
+        faces = []
+        for im, box_im, path_im in zip(img, batch_boxes, save_path):
+            if box_im is None:
+                faces.append(None)
+                continue
+
+
+            faces_im = []
+            for i, box in enumerate(box_im):
+                face_path = path_im
+                if path_im is not None and i > 0:
+                    save_name, ext = os.path.splitext(path_im)
+                    face_path = save_name + '_' + str(i + 1) + ext
+
+                face = extract_face(im, box, self.output_size, self.margin, face_path)
+                face = fixed_image_standardization(face)
+                faces_im.append(face)
+
+            faces_im = faces_im[0]
+
+            faces.append(faces_im)
+
+        if not batch_mode:
+            faces = faces[0]
+
+        return faces
+detector = Detector(model_type="dsfd", device="cpu", image_size=1000, output_size=128)
+x = torch.rand(640, 480 , 3)
+x = x.numpy().astype(np.uint8)
+st= time.time()
+boxes, faces = detector.detect_with_extractor(x)
+print(time.time()-st)
---
+++ Detector/dsfd/face_detection/dsfd/detect.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+import typing
+from .face_ssd import SSD
+from .config import resnet152_model_config
+from .. import torch_utils
+from torch.hub import load_state_dict_from_url
+from ..base import Detector
+from ..build import DETECTOR_REGISTRY
+
+model_url = "https://api.loke.aws.unit.no/dlr-gui-backend-resources-content/v2/contents/links/61be4ec7-8c11-4a4a-a9f4-827144e4ab4f0c2764c1-80a0-4083-bbfa-68419f889b80e4692358-979b-458e-97da-c1a1660b3314"
+
+
+@DETECTOR_REGISTRY.register_module
+class DSFDDetector(Detector):
+
+    def __init__(
+            self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        state_dict = load_state_dict_from_url(
+            model_url,
+            map_location=self.device,
+            progress=True)
+        self.net = SSD(resnet152_model_config)
+        self.net.load_state_dict(state_dict)
+        self.net.eval()
+        self.net = self.net.to(self.device)
+
+    @torch.no_grad()
+    def _detect(self, x: torch.Tensor,) -> typing.List[np.ndarray]:
+        """Batched detect
+        Args:
+            image (np.ndarray): shape [N, H, W, 3]
+        Returns:
+            boxes: list of length N with shape [num_boxes, 5] per element
+        """
+        # Expects BGR
+        x = x[:, [2, 1, 0], :, :]
+        with torch.cuda.amp.autocast(enabled=self.fp16_inference):
+            boxes = self.net(
+                x, self.confidence_threshold, self.nms_iou_threshold
+            )
+        return boxes
